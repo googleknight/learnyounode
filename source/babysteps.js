0f588553ccc88d23cdc35df4f91224baf8e4fe3d
Shubham Mathur <shubham.mathur@engineer.com>
2018-01-24 13:01:34 +0530

    Baby steps added

---
+++ source/babysteps.js
@@ -0,0 +1,7 @@
+const args = process.argv.slice(2);
+function sum(numbers) {
+  if (typeof numbers === 'undefined' || numbers.length === 0) { return 0; }
+  return numbers.reduce((first, second) => Number(first) + Number(second));
+}
+console.log(sum(args));
+module.exports = sum;
