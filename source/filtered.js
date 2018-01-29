e628af15063212c50f3048f899271fec4d5a837d
Shubham Mathur <shubham.mathur@engineer.com>
2018-01-30 05:28:31 +0530

    Test cases added and code updated for async

--- source/filtered.js
+++ source/filtered.js
@@ -1,15 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 
-let filteredFiles = null;
-function filterFiles(directoryPath, filterFileType) {
-  if (directoryPath && filterFileType) {
-    fs.readdir(directoryPath, (err, filepaths) => {
-      filteredFiles = filepaths.filter(file => path.extname(file) === `.${filterFileType}`);
-      filteredFiles.forEach((filename) => { console.log(filename); });
-    });
-  }
-  return filteredFiles;
+function checkFileType(fileName, filterFileType) {
+  return path.extname(fileName) === `.${filterFileType}`;
 }
-filterFiles(process.argv[2], process.argv[3]);
-module.exports = filterFiles;
+function printFileNames(filesList) {
+  filesList.forEach((filename) => { console.log(filename); });
+}
+function fileFilter(directoryPath, filterFileType, callback = printFileNames) {
+  fs.readdir(directoryPath, (err, filepaths) => {
+    if (err) {
+      return callback(err.code);
+    }
+    if (filterFileType === null || filterFileType.length < 1) {
+      return callback('No file type specified.');
+    }
+    const filteredFiles = filepaths.filter(fileName => checkFileType(fileName, filterFileType));
+    callback(filteredFiles);
+  });
+}
+
+// filterFiles(process.argv[2], process.argv[3]);
+module.exports = { fileFilter, checkFileType };
--- test/filtered.test.js
+++ test/filtered.test.js
@@ -1,32 +1,67 @@
-const fileFilter = require('../source/filtered');
+const filtered = require('../source/filtered');
 
 const directoryPath = 'test';
 const expectedFile = ['testfile.txt'];
 const skippedFile = ['firstio.test.js'];
 describe('Reading directory to display and count number of files of .txt type', () => {
-  // it('It should count number of .txt files to be 4', () => {
-  //   expect(filtered(directorypath, 'txt').length).toBe(4);
-  // });
-  // it('Matching name of .txt files found ', () => {
-  //   const output = ['testfile1.txt', 'testfile1.txt', 'testfile1.txt', 'testfile.txt'];
-  //   expect(filtered(directorypath, 'txt')).toBe(output);
-  // });
-  it('It should return null for null directory path', () => {
-    expect(fileFilter(null, 'txt')).toBeNull();
+  it('It should count number of .txt files to be 4', (done) => {
+    function callback(filesList) {
+      expect(filesList.length).toBe(4);
+      done();
+    }
+    filtered.fileFilter(directoryPath, 'txt', callback);
   });
-  it('It should return null for null directory path', () => {
-    expect(fileFilter(null, 'txt')).toBeNull();
+
+  it('Matching name of .txt files found ', (done) => {
+    const output = ['testfile.1.txt', 'testfile.2.txt', 'testfile.3.txt', 'testfile.txt'];
+    function callback(filesList) {
+      expect(filesList).toEqual(expect.arrayContaining(output));
+      done();
+    }
+    filtered.fileFilter(directoryPath, 'txt', callback);
   });
-  it('It should return null for null or no filetype', () => {
-    expect(fileFilter(directoryPath)).toBeNull();
+
+  it('It should return error for invalid directory path', (done) => {
+    function callback(errorcode) {
+      expect(errorcode).toBe('ENOENT');
+      done();
+    }
+    filtered.fileFilter('', 'txt', callback);
+  });
+
+  it('It should return error for invalid file type', (done) => {
+    function callback(errormsg) {
+      expect(errormsg).toBe('No file type specified.');
+      done();
+    }
+    filtered.fileFilter(directoryPath, '', callback);
   });
 });
 
-// describe('Reading directory to filter files of .txt type', () => {
-//   it('Checking a paritcular file is included in list of files', () => {
-//     expect(fileFilter(directoryPath, 'txt')).toEqual(expect.arrayContaining(expectedFile));
-//   });
-//   it('Checking a paritcular file is excluded in list of files', () => {
-//     expect(fileFilter(directoryPath, 'txt')).not.toEqual(expect.arrayContaining(skippedFile));
-//   });
-// });
+describe('Reading directory to filter files of .txt type', () => {
+  it('Checking a paritcular file is included in list of files', (done) => {
+    function callback(filesList) {
+      expect(filesList).toEqual(expect.arrayContaining(expectedFile));
+      done();
+    }
+    filtered.fileFilter(directoryPath, 'txt', callback);
+  });
+
+  it('Checking a paritcular file is excluded in list of files', () => {
+    function callback(filesList) {
+      expect(filesList).not.toEqual(expect.arrayContaining(skippedFile));
+    }
+    filtered.fileFilter(directoryPath, 'txt', callback);
+  });
+});
+const fileName = 'abc.txt';
+const filterFileType = 'txt';
+describe('Function to check filetype', () => {
+  it('Checking for txt file type', () => {
+    expect(filtered.checkFileType(fileName, filterFileType)).toBeTruthy();
+  });
+
+  it('Return false for no file type', () => {
+    expect(filtered.checkFileType(fileName)).toBeFalsy();
+  });
+});
